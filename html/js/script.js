d61adcac4c78e271e569a51a9f610ee84ba4d692
yangyongjian <76436092@qq.com>
2015-06-11 01:49:55 +0800

    修改参数传递方式为url传参

--- html/js/script.js
+++ html/js/script.js
@@ -1,10 +1,22 @@
 var BrokerWeb = BrokerWeb || {
     apiBaseUrl: 'http://218.4.117.11:8093',
     brokerid: 0,
+    params: {},
+    search: '',
+
+    init: function(callback){
+        this.params = this.getParams();
+        this.brokerid = this.params.id;
+        this.recieverType = this.type;
+
+        callback();
+    },
+
     getParams: function () {
         var params = {};
 
         var href = window.location.href;
+        this.search = window.location.search;
 
         if (href.indexOf('?') == -1) {
             return [];
@@ -22,5 +34,48 @@ var BrokerWeb = BrokerWeb || {
         }
 
         return params;
+    },
+    /**
+     * 数字格式转换成千分位
+     *@param{Object}num
+     */
+    commafy: function(num) {
+        if ((num + "").trim() == "") {
+            return"";
+        }
+        if (isNaN(num)) {
+            return"";
+        }
+        num = num + "";
+        if (/^.*\..*$/.test(num)) {
+            varpointIndex = num.lastIndexOf(".");
+            varintPart = num.substring(0, pointIndex);
+            varpointPart = num.substring(pointIndex + 1, num.length);
+            intPart = intPart + "";
+            var re = /(-?\d+)(\d{3})/
+            while (re.test(intPart)) {
+                intPart = intPart.replace(re, "$1,$2")
+            }
+            num = intPart + "." + pointPart;
+        } else {
+            num = num + "";
+            var re = /(-?\d+)(\d{3})/
+            while (re.test(num)) {
+                num = num.replace(re, "$1,$2")
+            }
+        }
+        return num;
+    },
+    /**
+     * 去除千分位
+     *@param{Object}num
+     */
+    delcommafy: function(num) {
+        if ((num + "").Trim() == "") {
+            return"";
+        }
+        num = num.replace(/,/gi, '');
+        returnnum;
     }
 };
+
